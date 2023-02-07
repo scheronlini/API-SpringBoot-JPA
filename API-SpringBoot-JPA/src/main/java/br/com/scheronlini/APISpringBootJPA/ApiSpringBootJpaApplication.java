c645337441b913a5b4c8e5e35cff9ea766650167
scheronlini <scheronlini@gmail.com>
2023-02-06 22:43:34 -0300

    Projeto Criado

---
+++ API-SpringBoot-JPA/src/main/java/br/com/scheronlini/APISpringBootJPA/ApiSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.scheronlini.APISpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ API-SpringBoot-JPA/src/test/java/br/com/scheronlini/APISpringBootJPA/ApiSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scheronlini.APISpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
